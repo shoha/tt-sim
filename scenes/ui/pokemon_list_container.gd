ce3b3aecd2e6f0595e8b40bb06db64d9cbb184c5
Hannah Shore <lookuphannahlookup@gmail.com>
2026-01-11 11:24:55 -0800

    Small renames

--- scenes/ui/pokemon_list_container.gd
+++ scenes/ui/pokemon_list_container.gd
@@ -1,18 +1,20 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
+func _unhandled_key_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		$"../TogglePokemonList".button_pressed = false
 
 func _on_button_toggled(toggled_on: bool) -> void:
 	if(toggled_on):
 		show()
 	else:
+		$PokemonFilter.clear()
 		hide()
